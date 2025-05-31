4d4a3722429bd4da50292292e95b3896d2c93124
TiagoVR4 <tiagoavrodrigues4@gmail.com>
2025-05-31 10:18:32 +0100

    create a global structure

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: tiagovr4 <tiagovr4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 13:47:15 by tiagovr4          #+#    #+#             */
-/*   Updated: 2025/05/13 16:24:49 by tiagovr4         ###   ########.fr       */
+/*   Updated: 2025/05/30 16:56:36 by tiagovr4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,14 @@
 # include <signal.h>
 # include <stdlib.h>
 # include "libft/libft.h"
+
+typedef struct	s_message
+{
+	char	*buffer;
+	size_t	size;
+	size_t	len;
+	int		bit;
+	char	c;
+}				t_message;
 
 #endif
--- src/server.c
+++ src/server.c
@@ -6,11 +6,50 @@
 /*   By: tiagovr4 <tiagovr4@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 18:39:44 by tiagovr4          #+#    #+#             */
-/*   Updated: 2025/05/13 16:53:21 by tiagovr4         ###   ########.fr       */
+/*   Updated: 2025/05/30 18:51:49 by tiagovr4         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
+
+t_message	g_message;
+
+static void	reallocate_buffer(void)
+{
+	char	*new_buffer;
+	size_t	new_size;
+	
+	new_size = g_message.size == 0 ? 64 : g_message.size * 2;
+	new_buffer = malloc(new_size);
+	if (!new_buffer)
+	{
+		free(g_message.buffer);
+		ft_putstr_fd("Memory allocation error\n", 2);
+		exit(1);
+	}
+	if (g_message.buffer)
+		ft_memcpy(new_buffer, g_message.buffer, g_message.len);
+	free(g_message.buffer);
+	g_message.buffer = new_buffer;
+	g_message.size = new_size;
+}
+
+// This function collects all the characters before printing
+static void	add_char_to_buffer(char c)
+{
+	if (g_message.len >= g_message.size)
+		reallocate_buffer();
+	g_message.buffer[g_message.len++] = c;
+	if (c == '\0')
+	{
+		ft_printf("%s", g_message.buffer);
+		ft_putchar_fd('\n', 1);
+		free(g_message.buffer);
+		g_message.buffer = NULL;
+		g_message.size = 0;
+		g_message.len = 0;
+	}
+}
 
 // This funtion reconstructs the original message from the bits received.
 static void	handle_bit(int sig)
@@ -23,9 +62,7 @@ static void	handle_bit(int sig)
 	bit_position++;
 	if (bit_position == 8)
 	{
-		ft_putchar_fd(current_char, 1);
-		if (current_char == '\0')
-			ft_putchar_fd('\n', 1);
+		add_char_to_buffer(current_char);
 		bit_position = 0;
 		current_char = 0;
 	}
@@ -52,6 +89,12 @@ static void	setup_signals(void)
 
 int	main(void)
 {
+	g_message.buffer = NULL;
+	g_message.size = 0;
+	g_message.len = 0;
+	g_message.bit = 0;
+	g_message.c = 0;
+	
 	ft_printf("Server PID: %d\n", getpid());
 	setup_signals();
 	while (1)
